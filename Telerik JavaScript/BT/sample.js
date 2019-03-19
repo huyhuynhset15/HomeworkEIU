c3f426e65cc801a443efd74f782e9e08b5f7200d
hmhuy97 <hm.huy97@gmail.com>
2019-03-19 15:42:59 +0700

    Update

---
+++ Telerik JavaScript/BT/sample.js
@@ -0,0 +1,25 @@
+//Object
+
+//jQuery
+
+//JSON
+var student = {
+  name: 'Huy',
+  birthday: '21/11/1997',
+  address: 'Thu Dau Mot'
+};
+//class Student{public Student{}{} }
+let Student = fuction(name, birthday, address)
+this.fname = name;
+this.lname = '';
+this.birthday = birthday;
+this.getName = function(){
+
+  this.getName = function(){
+    return fname + ' ' + this.lname;
+  }
+}
+new Student('Ngoc', '1/1/1990', 'Thu Dau Mot'),
+new Student('Ngoc', '1/1/1990', 'Thu Dau Mot'),
+new Student('Ngoc', '1/1/1990', 'Thu Dau Mot'),
+//Practice
